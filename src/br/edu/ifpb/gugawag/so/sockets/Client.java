417b896ff8da8c4a8a56ec60278858f4266a72f4
Fabiana Beda <82904498+fabianabeda@users.noreply.github.com>
2023-09-04 17:08:03 -0300

    atividade finalizada

--- src/br/edu/ifpb/gugawag/so/sockets/Client.java
+++ src/br/edu/ifpb/gugawag/so/sockets/Client.java
@@ -1,18 +1,41 @@
 package br.edu.ifpb.gugawag.so.sockets;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client {
-
+public class NfsClient {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 7000);
-        Scanner in = new Scanner(socket.getInputStream());
-        while(in.hasNextLine()) {
-            System.out.println(in.nextLine());
+        System.out.println("== Cliente NFS ==");
+
+        Socket socket = new Socket("127.0.0.1", 7001);
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Comando (readdir/rename/create/remove): ");
+            String command = scanner.nextLine();
+            out.writeUTF(command);
+
+            switch (command) {
+                case "readdir":
+                    String fileList = in.readUTF();
+                    System.out.println("Lista de arquivos: " + fileList);
+                    break;
+                case "rename":
+                case "create":
+                case "remove":
+                    System.out.print("Nome do arquivo: ");
+                    String fileName = scanner.nextLine();
+                    out.writeUTF(fileName);
+                    String response = in.readUTF();
+                    System.out.println(response);
+                    break;
+                default:
+                    System.out.println("Comando inválido");
+            }
         }
-        in.close();
-        socket.close();
     }
 }
--- src/br/edu/ifpb/gugawag/so/sockets/Server.java
+++ src/br/edu/ifpb/gugawag/so/sockets/Server.java
@@ -1,23 +1,102 @@
 package br.edu.ifpb.gugawag.so.sockets;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Server {
+public class NfsServer {
+    private static List<String> files = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(7000);
-        System.out.println("==Servidor==");
-        while(true) {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Cliente " + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
+        System.out.println("== Servidor NFS ==");
 
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-            out.println("Tchau!");
-            out.close();
-            clientSocket.close();
+        // Configurar o socket do servidor
+        ServerSocket serverSocket = new ServerSocket(7001);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Cliente conectado: " + clientSocket.getInetAddress());
+
+            // Crie uma thread para lidar com cada cliente
+            Thread clientHandler = new Thread(new ClientHandler(clientSocket));
+            clientHandler.start();
+        }
+    }
+
+    static class ClientHandler implements Runnable {
+        private Socket clientSocket;
+        private DataInputStream in;
+        private DataOutputStream out;
+
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
         }
 
+        @Override
+        public void run() {
+            try {
+                in = new DataInputStream(clientSocket.getInputStream());
+                out = new DataOutputStream(clientSocket.getOutputStream());
+
+                while (true) {
+                    String request = in.readUTF();
+                    String[] parts = request.split(" ");
+
+                    String command = parts[0];
+                    switch (command) {
+                        case "readdir":
+                            listFiles();
+                            break;
+                        case "rename":
+                            renameFile(parts[1], parts[2]);
+                            break;
+                        case "create":
+                            createFile(parts[1]);
+                            break;
+                        case "remove":
+                            removeFile(parts[1]);
+                            break;
+                        default:
+                            out.writeUTF("Comando inválido");
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void listFiles() throws IOException {
+            out.writeUTF(String.join(",", files));
+        }
+
+        private void renameFile(String oldName, String newName) throws IOException {
+            if (files.contains(oldName)) {
+                files.remove(oldName);
+                files.add(newName);
+                out.writeUTF("Arquivo renomeado com sucesso.");
+            } else {
+                out.writeUTF("Arquivo não encontrado.");
+            }
+        }
+
+        private void createFile(String fileName) throws IOException {
+            if (!files.contains(fileName)) {
+                files.add(fileName);
+                out.writeUTF("Arquivo criado com sucesso.");
+            } else {
+                out.writeUTF("Arquivo já existe.");
+            }
+        }
+
+        private void removeFile(String fileName) throws IOException {
+            if (files.contains(fileName)) {
+                files.remove(fileName);
+                out.writeUTF("Arquivo removido com sucesso.");
+            } else {
+                out.writeUTF("Arquivo não encontrado.");
+            }
+        }
     }
 }
